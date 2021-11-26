639c326c221310a5c9ad36d02598d9cf03e0ad14
Chris Agbolade <yemluck@gmail.com>
2021-11-26 01:55:15 -0600

    function tested

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -11,8 +11,21 @@ function addToCollection(title, artist, yearPublished) {
     return collection.push(music);
 } // end function addToCollection.
 
-addToCollection('Davido', 'Dodo', 2013);
-addToCollection('Wizkid', 'Caro', 2011);
-addToCollection('Runtown', 'Mad over you', 2017)
+addToCollection('Ed Sheeran', 'Perfect', 2017);
+console.log('3a. Collection after running function addToCollection', collection);
 
-console.log('1. Collection after running function addToCollection', collection)
+addToCollection('Mayorukun ft. Davido', 'Bobo', 2018);
+console.log('3b. Collection after running function addToCollection', collection);
+
+addToCollection('Ed Sheeran', 'Shape of you', 2017);
+console.log('3c. Collection after running function addToCollection', collection);
+
+addToCollection('Runtown', 'Mad over you', 2017);
+console.log('3d. Collection after running function addToCollection', collection);
+
+addToCollection('Bad Wolves', 'Zombie', 2018);
+console.log('3e. Collection after running function addToCollection', collection);
+
+addToCollection('The Fray', 'How to save a life', 2005);
+console.log('3f. Collection after running function addToCollection', collection);
+
