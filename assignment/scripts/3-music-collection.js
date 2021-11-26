08f0e93044177cef90ad613efbce018b777f98f0
Chris Agbolade <yemluck@gmail.com>
2021-11-26 01:45:56 -0600

    function addToCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,18 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+    let music = {} //  end let music
+    music.title = title;
+    music.artist = artist;
+    music.yearPublished = yearPublished;
+
+    return collection.push(music);
+} // end function addToCollection.
+
+addToCollection('Davido', 'Dodo', 2013);
+addToCollection('Wizkid', 'Caro', 2011);
+addToCollection('Runtown', 'Mad over you', 2017)
+
+console.log('1. Collection after running function addToCollection', collection)
