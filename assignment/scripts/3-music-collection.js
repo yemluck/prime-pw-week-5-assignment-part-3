52611cb442fb412370d2ec148d991f3f5b868aae
Chris Agbolade <yemluck@gmail.com>
2021-11-27 12:36:53 -0600

    modification to addCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,29 +3,49 @@ console.log('***** Music Collection *****')
 let collection = [];
 
 function addToCollection(title, artist, yearPublished) {
-    let music = {} //  end let music
-    music.title = title;
-    music.artist = artist;
-    music.yearPublished = yearPublished;
-
-    return collection.push(music);
+    let album = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    } //  end let album
+    collection.push(album);
+    return album
 } // end function addToCollection.
 
-addToCollection('Ed Sheeran', 'Perfect', 2017);
-console.log('3a. Collection after running function addToCollection', collection);
+console.log(addToCollection('Ed Sheeran', 'Perfect', 2017));
 
-addToCollection('Mayorukun ft. Davido', 'Bobo', 2018);
-console.log('3b. Collection after running function addToCollection', collection);
+console.log(addToCollection('Mayorukun ft. Davido', 'Bobo', 2018));
 
-addToCollection('Ed Sheeran', 'Shape of you', 2017);
-console.log('3c. Collection after running function addToCollection', collection);
+console.log(addToCollection('Ed Sheeran', 'Shape of you', 2017));
 
-addToCollection('Runtown', 'Mad over you', 2017);
-console.log('3d. Collection after running function addToCollection', collection);
+console.log(addToCollection('Runtown', 'Mad over you', 2017));
 
-addToCollection('Bad Wolves', 'Zombie', 2018);
-console.log('3e. Collection after running function addToCollection', collection);
+console.log(addToCollection('Bad Wolves', 'Zombie', 2018));
 
-addToCollection('The Fray', 'How to save a life', 2005);
-console.log('3f. Collection after running function addToCollection', collection);
+console.log(addToCollection('The Fray', 'How to save a life', 2005));
 
+console.log(collection); // end test
+
+
+
+function showCollection(array){
+
+
+} // end showCollection
+
+
+
+
+
+
+
+
+
+/*
+Add a function named `showCollection`. This function should:
+  - Take in an array parameter. (This allows it to be reused to show any collection, like the results from the find or search.)
+  - Console.log the number of items in the array.
+  - Loop over the array and console.log each album's information formatted like: `TITLE by ARTIST, published in YEAR`.
+
+- Test the `showCollection` function.
+*/
